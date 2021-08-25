36b982855aed0ccfbaebeee8995979c8c5beecc2
Andrew <alo42@georgetown.edu>
2021-08-25 14:25:17 -0400

    updates index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const ethers = require('ethers');
 const rpcNode = "https://rinkeby.infura.io/v3/3b83a506f358431399e427135570f8e8";
-const walletKey = process.env.WALLET_KEY;
 const checkWithdrawalEligibility = require('./lib/check-withdrawal-eligibility');
-const openQAddress = '0x0843199D7D29b072e251BC9d828482857698B97B';
-const withdrawIssueDepositFunctionSignature = 'function withdrawIssueDeposit(string, address) public';
+const withdrawIssueDepositFunctionSignature = 'function withdrawIssueDeposit(string, string) public';
+
+const openQAddress = '0xB9479f72a819ca7DC03552a19EE4BE7fD7000B5d';
+const walletKey = process.env.WALLET_KEY;
 
 exports.handler = async (event) => {
     const canWithdraw = await checkWithdrawalEligibility(event.username, event.issueId, event.oauthToken);
@@ -13,7 +14,7 @@ exports.handler = async (event) => {
         const wallet = new ethers.Wallet(walletKey, provider);
         const contract = new ethers.Contract(openQAddress, [withdrawIssueDepositFunctionSignature], provider);
         const contractWithWallet = contract.connect(wallet);
-        const result = contractWithWallet.withdrawIssueDeposit(event.issueId, event.payoutAddress);
+        const result = contractWithWallet.withdrawIssueDeposit(event.issueId, event.username);
         return result;
     } else {
         return `User ${event.username} does not have permission to withdraw on issue ${event.issueId}`;
