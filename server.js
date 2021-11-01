603e827e15ced8903fa66afe07738504018489f5
FlacoJones <andrew@openq.dev>
2021-11-01 17:14:12 -0500

    adds eslintrc.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ethers = require('ethers');
-const cors = require("cors");
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const { abi: openqABI } = require('./artifacts/contracts/OpenQ.sol/OpenQ.json');
@@ -12,7 +12,7 @@ const getUserCanAssignAddress = require('./lib/check_user_owns_address');
 const getIssueIdFromUrl = require('./lib/issueUrlToId');
 
 // Setup environment
-require("dotenv").config();
+require('dotenv').config();
 
 // Configure Express server middleware
 const PORT = 8090;
@@ -23,88 +23,90 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-    res.send(`OpenQ address is: ${process.env.OPENQ_ADDRESS}`);
+	res.send(`OpenQ address is: ${process.env.OPENQ_ADDRESS}`);
 });
 
 app.get('/env', (req, res) => {
-    res.send(`${JSON.stringify(process.env.PROVIDER_URL)}`);
+	res.send(`${JSON.stringify(process.env.PROVIDER_URL)}`);
 });
 
 app.post('/claim', async (req, res) => {
-    const { issueUrl, payoutAddress } = req.body;
+	const { issueUrl, payoutAddress } = req.body;
 
-    const oauthToken = req.signedCookies.github_oauth_token;
+	const oauthToken = req.signedCookies.github_oauth_token;
 
-    if (typeof oauthToken == "undefined") {
-        const error = { issueId: null, canWithdraw: false, type: "NO_GITHUB_OAUTH_TOKEN", message: `No GitHub OAuth token. You must sign in.` };
-        return res.status(401).json(error);
-    }
+	if (typeof oauthToken == 'undefined') {
+		const error = { issueId: null, canWithdraw: false, type: 'NO_GITHUB_OAUTH_TOKEN', message: 'No GitHub OAuth token. You must sign in.' };
+		return res.status(401).json(error);
+	}
 
-    await checkWithdrawalEligibility(issueUrl, oauthToken)
-        .then(async result => {
-            const { canWithdraw, reason, type, issueId } = result;
+	await checkWithdrawalEligibility(issueUrl, oauthToken)
+		.then(async result => {
+			const { canWithdraw } = result;
 
-            if (canWithdraw) {
-                const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
-                const wallet = new ethers.Wallet(process.env.WALLET_KEY, provider);
-                const contract = new ethers.Contract(process.env.OPENQ_ADDRESS, openqABI, provider);
-                const contractWithWallet = contract.connect(wallet);
-                const issueId = await getIssueIdFromUrl(issueUrl, oauthToken);
-                const issueAddress = await contractWithWallet.issueToAddress(issueId);
+			if (canWithdraw) {
+				const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
+				const wallet = new ethers.Wallet(process.env.WALLET_KEY, provider);
+				const contract = new ethers.Contract(process.env.OPENQ_ADDRESS, openqABI, provider);
+				const contractWithWallet = contract.connect(wallet);
+				const issueId = await getIssueIdFromUrl(issueUrl, oauthToken);
 
-                const issueIsOpen = await contractWithWallet.issueIsOpen(issueId);
-                if (issueIsOpen) {
-                    const options = { gasLimit: 3000000 };
-                    const claimBountyResult = await contractWithWallet.claimBounty(issueId, payoutAddress, options);
-                    res.status(200).json(result);
-                } else {
-                    const closer = await getIssueCloser(issueId, oauthToken);
-                    console.log(closer);
-                    res.status(401).json({ canWithdraw: false, type: "ISSUE_IS_CLAIMED", message: `The issue you are attempting to claim has already been claimed by GitHub user ${closer} with the address ${payoutAddress}.` });
-                }
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            return res.status(401).json(error);
-        });
+				const issueIsOpen = await contractWithWallet.issueIsOpen(issueId);
+				if (issueIsOpen) {
+					const options = { gasLimit: 3000000 };
+					await contractWithWallet.claimBounty(issueId, payoutAddress, options);
+					res.status(200).json(result);
+				} else {
+					/* This codepath is only possible if:
+											 A) A user tries to claim an issue twice
+											 B) Someone got the true closers GitHub OAuth token and closed the issue, sending funds to their own address
+										*/
+					const closer = await getIssueCloser(issueId, oauthToken);
+					res.status(401).json({ canWithdraw: false, type: 'ISSUE_IS_CLAIMED', message: `The issue you are attempting to claim has already been closed by ${closer} and sent to the address: ${payoutAddress}.` });
+				}
+			}
+		})
+		.catch(error => {
+			console.log(error);
+			return res.status(401).json(error);
+		});
 });
 
 app.post('/register', async (req, res) => {
-    const { username, oauthToken, address } = req.body;
+	const { username, oauthToken, address } = req.body;
 
-    getUserCanAssignAddress(username, oauthToken, address)
-        .then(canRegister => {
-            if (canRegister) {
-                const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
-                const wallet = new ethers.Wallet(process.env.WALLET_KEY, provider);
-                const contract = new ethers.Contract(process.env.OPENQ_ADDRESS, openqABI, provider);
-                const contractWithWallet = contract.connect(wallet);
-                const result = contractWithWallet.registerUserAddress(username, address);
-                console.log(result);
-                res.send(result);
-            } else {
-                res.send(`User ${username} does not have permission to register address ${address}.`);
-            }
-        })
-        .catch(error => {
-            res.send(error);
-        });
+	getUserCanAssignAddress(username, oauthToken, address)
+		.then(canRegister => {
+			if (canRegister) {
+				const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
+				const wallet = new ethers.Wallet(process.env.WALLET_KEY, provider);
+				const contract = new ethers.Contract(process.env.OPENQ_ADDRESS, openqABI, provider);
+				const contractWithWallet = contract.connect(wallet);
+				const result = contractWithWallet.registerUserAddress(username, address);
+				console.log(result);
+				res.send(result);
+			} else {
+				res.send(`User ${username} does not have permission to register address ${address}.`);
+			}
+		})
+		.catch(error => {
+			res.send(error);
+		});
 });
 
 app.post('/issueUrlToId', async (req, res) => {
-    const { issueUrl, token } = req.body;
+	const { issueUrl, token } = req.body;
 
-    getIssueIdFromUrl(issueUrl, token)
-        .then(response => {
-            res.send(response);
-        })
-        .catch(error => {
-            if (error == "NOT_FOUND") {
-                res.statusCode = 404;
-            }
-            res.send(error);
-        });
+	getIssueIdFromUrl(issueUrl, token)
+		.then(response => {
+			res.send(response);
+		})
+		.catch(error => {
+			if (error == 'NOT_FOUND') {
+				res.statusCode = 404;
+			}
+			res.send(error);
+		});
 });
 
 app.listen(PORT);
