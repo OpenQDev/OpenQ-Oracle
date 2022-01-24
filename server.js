47e351b1900985602f13bf7b6fd9a7d7e25cacbf
Flaco <andrew@openq.dev>
2022-01-24 13:14:36 -0600

    removes oracle logic, as this now lives in OZ Autotask

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ethers = require('ethers');
+const axios = require('axios');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
@@ -18,63 +19,30 @@ require('dotenv').config();
 const PORT = 8090;
 const app = express();
 app.use(cors({ credentials: true, origin: process.env.ORIGIN_URL }));
-app.use(cookieParser('entropydfnjd23'));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.post('/claim', async (req, res) => {
 	const { issueUrl, payoutAddress } = req.body;
 
 	console.log({ level: 'trace', id: payoutAddress, message: `${payoutAddress} attempting to withdraw issue at ${issueUrl}` });
+	const encryptedOauthToken = req.cookies.github_oauth_token;
 
-	const oauthToken = req.signedCookies.github_oauth_token;
-
-	if (typeof oauthToken == 'undefined') {
-		const error = { level: 'error', id: payoutAddress, canWithdraw: false, type: 'NO_GITHUB_OAUTH_TOKEN', message: 'No GitHub OAuth token. You must sign in.' };
-		console.error(error);
-		return res.status(401).json(error);
-	}
-
-	await checkWithdrawalEligibility(issueUrl, oauthToken)
-		.then(async result => {
-			const { canWithdraw } = result;
-
-			if (canWithdraw) {
-				const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
-				const wallet = new ethers.Wallet(process.env.CLIENT, provider);
-				const contract = new ethers.Contract(process.env.OPENQ_ADDRESS, openqABI, provider);
-				const contractWithWallet = contract.connect(wallet);
-				const { issueId, viewer } = await getIssueIdFromUrl(issueUrl, oauthToken);
-
-				const issueIsOpen = await contractWithWallet.bountyIsOpen(issueId);
-				if (issueIsOpen) {
-					const options = { gasLimit: 3000000 };
-
-					const txnResponse = await contractWithWallet.claimBounty(issueId, payoutAddress, options);
-					const txnReceipt = await txnResponse.wait();
-
-					for (const log of txnReceipt.logs) {
-						if (log.topics[0] == '0xe8bca3c18ca1c7ab481718f184745e0be969b9ff855fc58d9a483562ec9c960d') {
-							console.log('IssueClosed log: ', log);
-						}
-					}
-
-					const { transactionHash } = txnReceipt;
-					res.status(200).json({ issueId, payoutAddress, issueUrl, transactionHash });
-					console.log({ level: 'trace', id: payoutAddress, message: `${payoutAddress} successfully withdrawn on ${issueUrl}` });
-				} else {
-					const closer = await getIssueCloser(issueId, oauthToken);
-					const error = { level: 'error', canWithdraw: false, id: payoutAddress, type: 'ISSUE_IS_CLAIMED', message: `The issue you are attempting to claim as ${viewer} at url ${issueUrl} has already been closed by ${closer} and sent to the address ${payoutAddress}.` };
-					console.error(error);
-					res.status(401).json(error);
-				}
-			}
-		})
-		.catch(e => {
-			const error = { level: 'error', id: payoutAddress, type: e.type, message: e.message, canWithdraw: false };
-			console.error(error);
-			return res.status(401).json(error);
-		});
+	axios.post(process.env.OZ_CLAIM_AUTOTASK_URL, {
+		issueUrl,
+		payoutAddress
+	}, {
+		headers: {
+			'X-Authorization': encryptedOauthToken
+		}
+	}).then(result => {
+		const { txnHash, issueId } = result.txn;
+		console.log(result);
+		res.status(200).json({ issueId, payoutAddress, issueUrl, txnHash });
+	}).catch(error => {
+		res.status(500).json(error);
+	});
 });
 
 app.post('/register', async (req, res) => {
