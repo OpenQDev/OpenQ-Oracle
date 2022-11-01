2a0fb0a9e8fb799d51cca883e60be89d712f7102
FlacoJones <andrew@openq.dev>
2022-10-31 21:45:37 -0500

    adds test for associateUserIdToAddress

--- associateUserIdToAddress.js
+++ associateUserIdToAddress.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const axios = require('axios');
 
-const associateUserIdToAddress = async (userId, userAddress) => {
+const associateUserIdToAddress = async (userId, userAddress, signedOauthToken) => {
 	// Autotask returns 200 even if a promise is rejected
 	// We lower-case the x-authorization header because HTTP-Headers are case insensitive and some packages default to lower-casing
 	// Important for us, since in the Autotask we use case-sensitive dictionary lookup like event.request.headers['x-authorization']
