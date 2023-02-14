851da09d436bc95072d96ac1277c4304b3692c82
FlacoJones <andrew@openq.dev>
2023-02-14 16:51:28 -0600

    updates

--- associateUserIdToAddress.js
+++ associateUserIdToAddress.js
@@ -5,7 +5,19 @@ const associateUserIdToAddress = async (userId, userAddress, signedOauthToken, s
 	// Autotask returns 200 even if a promise is rejected
 	// We lower-case the x-authorization header because HTTP-Headers are case insensitive and some packages default to lower-casing
 	// Important for us, since in the Autotask we use case-sensitive dictionary lookup like event.request.headers['x-authorization']
-	return axios.post(process.env.OZ_USER_REGISTRATION_AUTOTASK_URL, { userId, userAddress }, { headers: { 'x-authorization': signedOauthToken, 'signature': signature } });
+	const headers = { 'x-authorization': signedOauthToken, 'signature': signature }
+	console.log('headers', headers)
+	
+	const config = { headers }
+	
+	console.log('config', config)
+	
+	return axios.post(
+			process.env.OZ_USER_REGISTRATION_AUTOTASK_URL, 
+			{ userId, userAddress }, 
+			config
+		);
 };
 
 module.exports = associateUserIdToAddress;
+// git add . && git commit --amend --no-edit && git push -f
